2d218b647319f7cdfa79c695ef7207bd1b4366de
Suminda De Silva <sumindaniro@gmail.com>
2020-10-11 00:54:09 +0530

    Added ENV Vars

--- aws-serverless-ci-cd-backend/badge-router.js
+++ aws-serverless-ci-cd-backend/badge-router.js
@@ -1,11 +1,13 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
 
     console.log(event);
 
-    // TODO implement
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('https://dev-475190109047-codepipeline-s3bucket.s3-us-west-2.amazonaws.com/badges/main-Build-passing-green.svg'),
+        statusCode: 301,
+        headers: {
+            Location: 'https://dev-475190109047-codepipeline-s3bucket.s3-us-west-2.amazonaws.com/badges/main-Build-passing-green.svg'
+        }
     };
+    
     return response;
 };

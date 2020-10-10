51e4831a388130ff7f8b5bc8a86e287c3716feb7
Suminda De Silva <sumindaniro@gmail.com>
2020-10-11 00:27:39 +0530

    Updated badge-router to return hardcoded image url

--- aws-serverless-ci-cd-backend/badge-router.js
+++ aws-serverless-ci-cd-backend/badge-router.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('https://dev-475190109047-codepipeline-s3bucket.s3-us-west-2.amazonaws.com/badges/main-Build-passing-green.svg'),
     };
     return response;
 };
